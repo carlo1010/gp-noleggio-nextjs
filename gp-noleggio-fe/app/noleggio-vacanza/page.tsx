ddea24f409e20db9c80072c34db8603940c7835a
2209raffae <2209raffae@gmail.com>
2025-12-13 12:30:35 +0100

    Aggiunta seconda pagina /noleggio-vacanza
    aggiunta seconda pagine serve solo cambiare il footer e aggiungere un icona sopra la search card

---
+++ gp-noleggio-fe/app/noleggio-vacanza/page.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/header";
+import OfferBar from "@/components/offer-banner";
+import HeroVacanzaSection from "@/components/hero-vacanza";
+import ContentSection from "@/components/content-section";
+import VantaggiIcone from "@/components/vantaggi-icone";
+import TerminiCondizioni from "@/components/termini-condizioni";
+
+export default function Page() {
+    return (
+        <>
+            <Header/>
+            <OfferBar/>
+            <HeroVacanzaSection/>
+            <ContentSection/>
+            <VantaggiIcone/>
+            <TerminiCondizioni/>
+        </>
+    );
+}
